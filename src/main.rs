44303fe692a4da5b689a995b9fabf6a1f1602cf0
Pouey Ludwig <gogopex@gmail.com>
2020-08-10 23:40:07 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,97 @@
 use anyhow::{Error, Result};
-use io::Write;
-use scrypt::ScryptParams;
 use std::io;
+use io::{Read, Write, BufReader};
+use structopt::clap::AppSettings;
 use structopt::StructOpt;
-use std::fs::File;
+use std::fs::{File};
+use secrecy::Secret;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "psswd", about="small password manager")]
+#[structopt(setting = AppSettings::InferSubcommands)]
 enum Opt {
-    /// Verbose mode (-v, -vv, -vvv, etc.)
-    // #[structopt(short, long, parse(from_occurrences))]
-    // verbose: u8,
-
-    // #[structopt(short, long)]
-    // add: Add,
+    #[structopt(alias = "insert")]
     Add,
-    Show,
+    #[structopt(alias = "list")]
+    Show {
+        filename: String
+    },
 }
 
 fn main() {
     let opt = Opt::from_args();
     match opt {
-        Opt::Add => add_entry(),
-        Opt::Show => show(),
+        Opt::Add => add(),
+        Opt::Show { filename } => show(&filename)
     };
-
-    // if opt.new {
-    //     println!("test");
-    // }
-    // println!("{:#?}", opt);
 }
 
-fn add_entry() -> Result<(), Error> {
+fn add() -> Result<(), Error> {
     println!("Enter your credential: ");
     io::stdout().flush()?;
 
-    let mut entry = String::new();
-    io::stdin().read_line(&mut entry)?;
+    let mut credential = String::new();
+    io::stdin().read_line(&mut credential)?;
 
     let password = rpassword::prompt_password_stdout("Enter a password: ")?;
+    let passphrase = rpassword::prompt_password_stdout("Enter a passphrase: ")?;
 
-    let params = ScryptParams::recommended();
-    // let encrypted = scrypt::scrypt_simple(&password, &params).unwrap();
-    let encrypted = scrypt::scrypt_simple(&password, &params).unwrap();
-    
-    let home_dir = dirs::home_dir().unwrap().into_os_string().into_string().unwrap();
+    let encrypted_pwd = encrypt(password, passphrase);
 
-    let mut file = File::create(format!("{}/test.txt", home_dir))?;
-    file.write(&encrypted.as_bytes());
+    // io::stdout().flush()?;
 
+    // println!("Enter your filename: ");
+    // let filename = String::new();
+    // io::stdin().read_line(&mut credential)?;
+
+    let mut file = File::create(format!("{}/{}.txt", home_dir(), "test"))?;
+    file.write(&encrypted_pwd?);
     
     Ok(())
 }
 
-fn show() -> Result<(), Error> {
-    // unimplemented!()
-    let verif = scrypt::scrypt_check("test", &encrypted).is_ok();
+fn show(filename: &str) -> Result<(), Error> {
+    let file = File::open(format!("{}/{}.txt",  home_dir(), filename))?;
+    let mut buffer = BufReader::new(file);
+    
+    let mut encrypted = vec![];
+    buffer.read_to_end(&mut encrypted)?;
+    
+    let passphrase = rpassword::prompt_password_stdout("Enter your passphrase: ")?;
+    let decrypted_pwd = decrypt(&encrypted, passphrase)?;
+    let decrypted_pwd = String::from_utf8(decrypted_pwd)?;
+    
+    println!("{}", decrypted_pwd);
     Ok(())    
+}
+
+fn encrypt(password: String, passphrase: String) -> Result<Vec<u8>, Error> {
+    let encryptor = age::Encryptor::with_user_passphrase(Secret::new(passphrase));
+
+    let mut encrypted = vec![];
+    let mut writer = encryptor.wrap_output(&mut encrypted, age::Format::Binary)?;
+    writer.write_all(&password.as_bytes())?;
+    writer.finish()?;
+
+    Ok(encrypted)
+}
+
+fn decrypt(encrypted_pwd: &[u8], passphrase: String) -> Result<Vec<u8>, Error> {
+    let decryptor = match age::Decryptor::new(&encrypted_pwd[..])? {
+        age::Decryptor::Passphrase(d) => d,
+        _ => unreachable!(),
+    };
+
+    let mut decrypted = vec![];
+    let mut reader = decryptor.decrypt(&Secret::new(passphrase), None)?;
+    reader.read_to_end(&mut decrypted)?;
+
+    Ok(decrypted)
+}
+
+fn home_dir() -> String {
+    dirs::home_dir()
+    .unwrap()
+    .into_os_string()
+    .into_string()
+    .unwrap()
 }
